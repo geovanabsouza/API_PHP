1234997f198ef06d44b4c25b3f5e9a401e389cf6
geovanabsouza <geovanabsouza08@gmail.com>
2026-04-16 10:15:42 -0300

    16/04

--- Aula 01/api.php
+++ Aula 01/api.php
@@ -5,14 +5,42 @@
 
     $metodo = $_SERVER['REQUEST_METHOD'];
 
+    $arquivo = 'usuarios.json';
+
+    if (!file_exists($arquivo)) {
+        file_put_contents($arquivo, json_encode([], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    }
+
+    $usuarios = json_decode(file_get_contents($arquivo), true);
+
+    //$usuarios = [
+        //["id" => 1, "nome" => "Maria Souza", "email" => "maria@gmail.com"],
+        //["id" => 2, "nome" => "João Silva", "email" => "joao@gmail.com"]
+    //];
+
+
     //echo "Método da Requisição: " . $metodo;
 
     switch ($metodo) {
         case 'GET':
-            echo "AQUI AÇÕES DO MÉTODO GET";
+            echo json_encode($usuarios);
+            //echo "AQUI AÇÕES DO MÉTODO GET";
             break;
         case 'POST':
-            echo "AQUI AÇÕES DO MÉTODO POST";
+            //echo "AQUI AÇÕES DO MÉTODO POST";
+            $dados = json_decode(file_get_contents('php://input'), true);
+            //print_r($dados);
+            $novoUsuario = [
+                "id" => $dados["id"],
+                "nome" => $dados["nome"],
+                "email" => $dados["email"]
+            ];
+
+            //Adiciona o novo usuário ao array existente
+            array_push($usuarios, $novoUsuario);
+            echo json_encode('Usuário inserido com sucesso!');
+            print_r($usuarios);
+
             break;
         default :
             echo "MÉTODO NÃO ENCONTRADO!";
@@ -20,13 +48,8 @@
     }
 
     //CONTEÚDO
-    //$usuarios = [
-       //["id" => 1, "nome" => "Maria Souza", "email" => "maria@gmail.com"],
-       //["id" => 2, "nome" => "João Silva", "email" => "joao@gmail.com"]
-    //];
-
+ 
     //Converte para json e retorna
-
 
 
 
